b904d1988ada45a264d195d2d17d99a3d2fb0286
Magnus Conrad Hyll <magnus@hyll.no>
2018-09-30 13:58:57 +0200

    Øving 5 og 6

---
+++ oving_6/oppg_c/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> permutate() {
+    std::vector<std::string> permutations;
+
+    return permutations;
+}
+
+int main() {
+    std::cout << "Hello kis\n";
+}
